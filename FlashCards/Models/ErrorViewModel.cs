bdb823fa52654f2fe683de33a10aad2fc693e00e
maksym <maksym@DESKTOP-B5H57AK>
2024-02-06 12:30:11 +0200

    Add project files.

---
+++ FlashCards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlashCards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
