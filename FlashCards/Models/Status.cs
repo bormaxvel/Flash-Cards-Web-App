ffb28bb36fc04bf203fe4b536818e0bc68851ba4
Артем Большаков <artemis4650@gmail.com>
2024-02-09 10:51:07 +0200

    status model

---
+++ FlashCards/Models/Status.cs
@@ -0,0 +1,27 @@
+﻿namespace FlashCards.Models
+{
+    public class Status
+    {
+        // Ідентифікатор
+        public int StatusId { get; set; }
+
+        // Ідентифікатор користувача
+        public int UserId { get; set; }
+
+        // Ідентифікатор слова
+        public int CardId { get; set; }
+
+        // Кількість згадок (скільки разів поспіль користувач згадав слово)
+        public int Mentions { get; set; }
+
+        // Чи було слово взято для вивчення?
+        public bool IsWordTakenForLearning { get; set; }
+
+        // Чи вивчено слово?
+        public bool IsWordLearned { get; set; }
+
+        // Чи було слово відоме раніше?
+        public bool IsWordKnownBefore { get; set; }
+
+    }
+}
