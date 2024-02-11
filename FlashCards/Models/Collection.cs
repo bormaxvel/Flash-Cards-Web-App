5efb37f59e5570640246373c457024f6f72660d0
Dmytro Vovk <dimavovk688@gmail.com>
2024-02-11 12:14:55 +0200

    Dmytro Commit

---
+++ FlashCards/Models/Collection.cs
@@ -0,0 +1,16 @@
+﻿namespace FlashCards.Models
+{
+    public class User
+    {
+
+        // Ідентифікатор колекції
+        public int id { get; set; }
+
+        // Назва колекції
+        public string name { get; set; }
+
+        // Опис колекції
+        public string description { get; set; }
+
+    }
+}
---
+++ FlashCards/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace FlashCards.Models
+{
+    public class User
+    {
+
+        // Ідентифікатор користувача
+        public int userId{ get; set; }
+
+        // Нікнейм користувача
+        public string nickName { get; set; }
+
+    }
+}
