b0caf0bad7c27ea9318458a699726b2e93955c75
Denis Bolshakov <artemis0564@gmail.com>
2024-02-11 08:27:47 +0200

    Denys commit

---
+++ FlashCards/Models/cardCollectionLink.cs
@@ -0,0 +1,17 @@
+﻿namespace FlashCards.Models
+{
+    public class cardCollectionLink
+    {
+        // Ідентифікатор
+        public int cardCollectionLinkId { get; set; }
+        //ід картки
+        public int CardId { get; set; }
+        //ід назви
+        public int CollectionID { get; set; }
+
+        public Card Card { get; set; }
+
+        public Collection Collection { get; set; }
+
+    }
+}
---
+++ FlashCards/Models/userCollectionLink.cs
@@ -0,0 +1,16 @@
+﻿namespace FlashCards.Models
+{
+    public class userCollectionLink
+    {
+        //ід
+        public int userCollectionLinkId { get; set; }
+        //ід користувача
+        public int UserId { get; set; }
+        //ід колекції
+        public int CollectionID { get; set; }
+
+        public Collection Collection { get; set; }
+
+        public User User { get; set; }
+    }
+}
