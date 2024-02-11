0e56dcc786b0e9baff84c6e9fdf536de6487364f
Imaksymov <90256463+Imaksymov@users.noreply.github.com>
2024-02-11 16:13:30 +0200

    Add files via upload

---
+++ FlashCards/Models/Card.cs
@@ -0,0 +1,16 @@
+namespace FlashCards.Models
+{
+    public class Card
+    {
+
+        // Ідентифікатор картки
+        public int Id { get; set; }
+        // Термін (оригінал слова)
+        public string Term { get; set; }
+        // Визначення (переклад)
+        public string Definition { get; set; }
+        // Контекст (як використовується)
+        public string Context { get; set; }
+
+    }
+}
