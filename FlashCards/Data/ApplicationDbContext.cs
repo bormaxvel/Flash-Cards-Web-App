f3cc359492bc4ede2784a9c970d4005f2d280add
Dmytro Vovk <dimavovk688@gmail.com>
2024-02-15 17:01:47 +0200

    Dmytro commit

--- FlashCards/Data/ApplicationDbContext.cs
+++ FlashCards/Data/ApplicationDbContext.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FlashCards.Models;
 
 namespace FlashCards.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<cardCollectionLink> CardCollectionLinks{ get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<userCollectionLink> UserCollectionLinks { get; set; }
+
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        
     }
 }
