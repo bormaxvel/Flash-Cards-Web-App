f2890e06dc3afcb643e490b747f27739cd787c12
Ivan Maksymov <wano20022@gmail.com>
2024-03-11 10:35:50 +0200

    Home page changes

--- FlashCards/Controllers/HomeController.cs
+++ FlashCards/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using FlashCards.Data;
 using FlashCards.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FlashCards.Controllers
@@ -7,15 +9,17 @@ namespace FlashCards.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Collections.ToListAsync());
         }
 
         public IActionResult Privacy()
